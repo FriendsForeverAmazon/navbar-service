e638807661d2728b9d6e52739b3c45114f6a53fc
Joseph Spear <jcspear90@gmail.com>
2019-01-11 17:06:22 -0800

    Fixed mongo CRUD operations

--- SDC-server/mongo/controller/product.controller.js
+++ SDC-server/mongo/controller/product.controller.js
@@ -1,9 +1,45 @@
-const { Product } = require('../models/product.model');
-const { Category } = require('../models/category.model');
+var { Product } = require('../models/product.model');
+var { Category } = require('../models/category.model');
 
-const get = (req, res) => {
-  const categoryParam = req.params.category.toLowerCase();
-  const { query } = req.params
+var get = (req, res) => {
+  var categoryParam = req.params.category.toLowerCase();
+  var { query } = req.params
+
+  if (categoryParam === 'all departments') {
+    Product.find({
+          name: 
+            new RegExp(query.toLowerCase(),'g')
+        })
+      .then(products => {
+        res.status(200).send({ products });
+      })
+      .catch(err => {
+        res.status(500).send({ err });
+      });
+  } else {
+    Category.find({
+        category: 
+          categoryParam
+      })
+      .then(category => {
+        if (category) {
+          return Product.find({
+            categoryId: category.id,
+            name: 
+              new RegExp(query.toLowerCase(),'g')
+          })
+        }
+        return [];
+      })
+      .then(products => {
+        res.status(200).send({ products });
+      })
+      .catch(err => {
+        res.status(500).send({ err });
+      })
+  }
+  var categoryParam = req.params.category.toLowerCase();
+  var { query } = req.params
 
   if (categoryParam === 'all departments') {
     Product.find({
@@ -40,9 +76,9 @@ const get = (req, res) => {
   }
 }
 
-const create = (req, res) => {
-  const categoryParam = req.params.category;
-  const { query } = req.params;
+var create = (req, res) => {
+  var categoryParam = req.params.category;
+  var { query } = req.params;
   Category.find({
     category: 
       categoryParam
@@ -64,10 +100,10 @@ const create = (req, res) => {
   })
 }
 
-const update = (req, res) => {
-  const categoryParam = req.params.category;
-  const oldProduct = req.params.oldProduct;
-  const newProduct = req.params.newProduct;
+var update = (req, res) => {
+  var categoryParam = req.params.category;
+  var oldProduct = req.params.oldProduct;
+  var newProduct = req.params.newProduct;
   Category.find({
     category: 
       categoryParam
@@ -90,9 +126,9 @@ const update = (req, res) => {
   })
 }
 
-const remove = (req, res) => {
-  const categoryParam = req.params.category;
-  const { query } = req.params;
+var remove = (req, res) => {
+  var categoryParam = req.params.category;
+  var { query } = req.params;
   Category.find({
     category: 
       categoryParam
