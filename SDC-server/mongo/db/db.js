d4ca30114d3b1920b47e2e1872f600889cb4a6e6
Joseph Spear <jcspear90@gmail.com>
2019-01-22 15:46:29 -0800

    Updated DB URL

--- SDC-server/mongo/db/db.js
+++ SDC-server/mongo/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 var Redis = require('ioredis');
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://3.88.197.225:27017/";
 
 const connection = mongoose.createConnection(url,{dbName:'navbar'});
 var cache = new Redis();
