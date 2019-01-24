98da698892c01241bcaaa1137d069e702ce23fe3
Joseph Spear <jcspear90@gmail.com>
2019-01-24 15:54:59 -0800

    Migrating redis to its own server

--- SDC-server/mongo/db/db.js
+++ SDC-server/mongo/db/db.js
@@ -3,6 +3,6 @@ var Redis = require('ioredis');
 const url = "mongodb://34.238.49.225:27017/";
 
 const connection = mongoose.createConnection(url,{dbName:'navbar'});
-var cache = new Redis();
+var cache = new Redis(6379, '54.163.152.68');
 
 module.exports = { connection, cache };
