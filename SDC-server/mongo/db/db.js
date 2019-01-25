47c8de9136c934a9e17d0e5b097edc6389f1ba89
Joseph Spear <jcspear90@gmail.com>
2019-01-24 17:22:06 -0800

    Implementing redis database server

--- SDC-server/mongo/db/db.js
+++ SDC-server/mongo/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 var Redis = require('ioredis');
 const url = "mongodb://34.238.49.225:27017/";
-var cache = new Redis("redis://54.163.152.68:6379");
+var cache = new Redis("redis://3.92.213.254:6379/");
 
 const connection = mongoose.createConnection(url,{dbName:'navbar'});
 
