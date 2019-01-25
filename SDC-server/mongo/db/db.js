6452d4b1d790eb008fc6dea38f79fce94a055196
Joseph Spear <jcspear90@gmail.com>
2019-01-24 16:46:53 -0800

    Correcting Redis address

--- SDC-server/mongo/db/db.js
+++ SDC-server/mongo/db/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 var Redis = require('ioredis');
 const url = "mongodb://34.238.49.225:27017/";
+var cache = new Redis("redis://54.163.152.68:6379");
 
 const connection = mongoose.createConnection(url,{dbName:'navbar'});
-var cache = new Redis(6379, '54.163.152.68');
 
 module.exports = { connection, cache };
